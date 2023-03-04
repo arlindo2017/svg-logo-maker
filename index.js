7fab2867700cc3273caf5ef556536a21938710cd
Arlindo Pinto <perensen@gmail.com>
2023-03-04 16:32:59 -0500

    circle class

--- index.js
+++ index.js
@@ -1,105 +1,63 @@
-const inquirer = require('inquirer');
+const { userPrompt } = require('./lib/prompt.js');
+const { Square, Circle, Triangle } = require('./lib/shapes.js');
 const fs = require('fs');
 
-function writeReadme(data) {
-  let projectLicense = data.license
-  if (projectLicense == 'Apache License 2.0') {
-    projectLicense = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-  } else if (projectLicense == 'MIT License') {
-    projectLicense = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-  } else {
-    projectLicense = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
-  }
+function init() {
+    userPrompt()
+    .then((answers) => {
+      const userText = answers.text;
+      const userTextColor = answers.textcolor;
+      const userShapeColor = answers.shapecolor;
+      const userShape = answers.shape;
 
-  return `# ${data.projectitle}
-  ${projectLicense}
-  
-  ## Description
-  ${data.description}
+      return {userText, userTextColor, userShapeColor, userShape};
+    })
+    .then(({userText, userTextColor, userShapeColor, userShape}) => {
+      let svgShape;
+      let svgShapeString;
+    
+      if (userShape[0] === 'Circle') {
+        //constructor(text, textcolor, shapecolor,shapetype, cx, cy, r)
+        svgShape = new Circle(userText, userTextColor, userShape[0], userShapeColor, 150 ,100, 80);
+        svgShapeString = `<circle cx="${svgShape.cx}" cy="${svgShape.cy}" r="${svgShape.r}" fill="${svgShape.shapecolor}"/>`
+        return svgShapeString;
 
-  ### Table of Contents
-  - [Description](#description)
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Contribution](#contribution)
-  - [Tests](#tests)
-  - [Questions](#questions)
+      } else if (userShape[0] === 'Triangle') {
+        svgShape = new Triangle(userText, userTextColor, userShape[0], userShapeColor,[200, 10, 250, 190, 160, 210]);
+        //svgShapeString = `<circle cx="${svgShape.cx}" cy="${svgShape.cy}" r="${svgShape.r}" fill="${svgShape.shapecolor}"/>`
+        
+        return svgShapeString;
 
-  ## Installation
-  ${data.installation}
+      } else if (userShape[0] === 'Square') {
+        //(text, textcolor, shapetype, shapecolor,x,y,width,height)
+        svgShape = new Square(userText, userTextColor, userShape[0], userShapeColor, 60, 10, 30, 30);
+        //svgShapeString = `<circle cx="${svgShape.cx}" cy="${svgShape.cy}" r="${svgShape.r}" fill="${svgShape.shapecolor}"/>`
+        
+        return svgShapeString;
 
-  ## Usage
-  ${data.usage}
+      } else {
+        throw new Error('Invalid shape selected');
+      }  
+    })
+    .then((svgShapeString) => {
+        fs.writeFileSync('./examples/logo.svg',
+        
+        svgShapeString
 
-  ## Contribution
-  ${data.contribution}
-
-  ## Tests
-  ${data.tests}
-
-  ## Questions
-  - See my work at GitHub: [github.com/${data.github}](#https://github.com/${data.github}/)
-  - If you have any questions you can email me at [${data.email}](mailto:${data.email})
-
-
-  ### This readme file was automatically generated using inquirer
-  `; 
+        );
+        
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'projectitle',
-      message: 'What is the title of your Project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Enter a project description',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Add project installation instructions',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Add project usage',
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'How to Contribute to this project',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Enter testing instructions',
-     },
-    {
-      type: 'checkbox',
-      name: 'license',
-      message: 'Select your license?',
-      choices: ['Apache License 2.0', 'MIT License', 'Mozilla Public License 2.0'],
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Your Github username',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Your email address',
-    },
-  ])
+init();
+
+
+
+
+// Do something with the new shape, e.g. write it to a file
+//fs.writeFileSync('newShape.json', JSON.stringify(newShape));
+
   
-  .then((data) => {
-    fs.writeFile('GENERATED-README.md',  writeReadme(data), (error) => {
-        error ? console.log(error): console.log('Success, your GENERATED-README.md file has been created!')
-     })
-});
-
-
