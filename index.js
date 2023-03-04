7a2d599faf97bc56c8b97e2d820d3c41dac888e1
Arlindo Pinto <perensen@gmail.com>
2023-03-04 17:40:56 -0500

    added file name from shape

--- index.js
+++ index.js
@@ -17,35 +17,39 @@ function init() {
       let svgShapeString;
       let svgText;
       let svgTextColor;
+      let svgShapeType;
 
       if (userShape[0] === 'Circle') {
         //constructor(text, textcolor, shapecolor,shapetype, cx, cy, r)
         svgShape = new Circle(userText, userTextColor, userShapeColor, userShape[0], 150, 100, 80);
         svgText = svgShape.text;
         svgTextColor = svgShape.textcolor;
+        svgShapeType = svgShape.shapetype;
         svgShapeString = `<circle cx="${svgShape.cx}" cy="${svgShape.cy}" r="${svgShape.r}" fill="${svgShape.shapecolor}"/>`;
-        return {svgShapeString, svgText, svgTextColor};
+        return {svgShapeString, svgText, svgTextColor, svgShapeType};
 
       } else if (userShape[0] === 'Triangle') {
         svgShape = new Triangle(userText, userTextColor, userShapeColor, userShape[0], [150, 10, 244, 190 ,56, 190]);
         svgText = svgShape.text;
         svgTextColor = svgShape.textcolor;
+        svgShapeType = svgShape.shapetype;
         svgShapeString = `<polygon points="${svgShape.points.join(' ')}" fill="${svgShape.shapecolor}"/>`;
-        return {svgShapeString, svgText, svgTextColor};
+        return {svgShapeString, svgText, svgTextColor, svgShapeType};
 
       } else if (userShape[0] === 'Square') {
         svgShape = new Square(userText, userTextColor, userShapeColor, userShape[0], 60, 10, 200, 200);
         svgTextColor = svgShape.textcolor;
         svgText = svgShape.text;
+        svgShapeType = svgShape.shapetype;
         svgShapeString = `<rect x="${svgShape.x}" y="${svgShape.y}" width="${svgShape.width}" height="${svgShape.height}" fill="${svgShape.shapecolor}"/>`;
-        return {svgShapeString, svgText, svgTextColor};
+        return {svgShapeString, svgText, svgTextColor, svgShapeType};
 
       } else {
         throw new Error('Invalid shape selected');
       }
     })
-    .then(({ svgShapeString, svgText, svgTextColor }) => {
-      fs.writeFileSync('./examples/logo.svg',
+    .then(({ svgShapeString, svgText, svgTextColor, svgShapeType}) => {
+      fs.writeFileSync(`./examples/${svgShapeType}.svg`,
         `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
        ${svgShapeString}
       <text x="150" y="125" font-size="60" text-anchor="middle" fill="${svgTextColor}">${svgText}</text>
