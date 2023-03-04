c73a05a5c5ee3ddd14e2e4ebf57c852f9c89911d
Arlindo Pinto <perensen@gmail.com>
2023-03-04 17:19:09 -0500

    added formatting to circle

--- index.js
+++ index.js
@@ -3,49 +3,58 @@ const { Square, Circle, Triangle } = require('./lib/shapes.js');
 const fs = require('fs');
 
 function init() {
-    userPrompt()
+  userPrompt()
     .then((answers) => {
       const userText = answers.text;
       const userTextColor = answers.textcolor;
       const userShapeColor = answers.shapecolor;
       const userShape = answers.shape;
 
-      return {userText, userTextColor, userShapeColor, userShape};
+      return { userText, userTextColor, userShapeColor, userShape };
     })
-    .then(({userText, userTextColor, userShapeColor, userShape}) => {
+    .then(({ userText, userTextColor, userShapeColor, userShape }) => {
       let svgShape;
       let svgShapeString;
-    
+      let svgText;
+      let svgTextColor;
+
       if (userShape[0] === 'Circle') {
         //constructor(text, textcolor, shapecolor,shapetype, cx, cy, r)
-        svgShape = new Circle(userText, userTextColor, userShape[0], userShapeColor, 150 ,100, 80);
-        svgShapeString = `<circle cx="${svgShape.cx}" cy="${svgShape.cy}" r="${svgShape.r}" fill="${svgShape.shapecolor}"/>`
-        return svgShapeString;
+        svgShape = new Circle(userText, userTextColor, userShapeColor, userShape[0], 150, 100, 80);
+        svgText = svgShape.text;
+        svgTextColor = svgShape.textcolor;
+        svgShapeString = `<circle cx="${svgShape.cx}" cy="${svgShape.cy}" r="${svgShape.r}" fill="${svgShape.shapecolor}"/>`;
+        console.log(svgShapeString)
+        return {svgShapeString, svgText, svgTextColor};
 
       } else if (userShape[0] === 'Triangle') {
-        svgShape = new Triangle(userText, userTextColor, userShape[0], userShapeColor,[200, 10, 250, 190, 160, 210]);
-        //svgShapeString = `<circle cx="${svgShape.cx}" cy="${svgShape.cy}" r="${svgShape.r}" fill="${svgShape.shapecolor}"/>`
-        
-        return svgShapeString;
+        svgText = svgShape.text;
+        svgShape = new Triangle(userText, userTextColor, userShapeColor, userShape[0], [200, 10, 250, 190, 160, 210]);
+        svgText = svgShape.text;
+        svgTextColor = svgShape.textcolor;
+        svgShapeString = `<polygon points="${svgShape.points.join(' ')}" fill="${svgShape.shapecolor}"/>`;
+        return {svgShapeString, svgText, svgTextColor};
 
       } else if (userShape[0] === 'Square') {
-        //(text, textcolor, shapetype, shapecolor,x,y,width,height)
-        svgShape = new Square(userText, userTextColor, userShape[0], userShapeColor, 60, 10, 30, 30);
-        //svgShapeString = `<circle cx="${svgShape.cx}" cy="${svgShape.cy}" r="${svgShape.r}" fill="${svgShape.shapecolor}"/>`
-        
-        return svgShapeString;
+        svgText = svgShape.text;
+        svgTextColor = svgShape.textcolor;
+        svgShape = new Square(userText, userTextColor, userShapeColor, userShape[0], 60, 10, 200, 200);
+        svgText = svgShape.text;
+        svgShapeString = `<rect x="${svgShape.x}" y="${svgShape.y}" width="${svgShape.width}" height="${svgShape.height}" fill="${svgShape.shapecolor}"/>`;
+        return {svgShapeString, svgText, svgTextColor};
 
       } else {
         throw new Error('Invalid shape selected');
-      }  
+      }
     })
-    .then((svgShapeString) => {
-        fs.writeFileSync('./examples/logo.svg',
-        
-        svgShapeString
+    .then(({ svgShapeString, svgText, svgTextColor }) => {
+      fs.writeFileSync('./examples/logo.svg',
+        `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+       ${svgShapeString}
+      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${svgTextColor}">${svgText}</text>
+      </svg>`
+      );
 
-        );
-        
     })
     .catch((err) => {
       console.log(err);
@@ -53,11 +62,3 @@ function init() {
 }
 
 init();
-
-
-
-
-// Do something with the new shape, e.g. write it to a file
-//fs.writeFileSync('newShape.json', JSON.stringify(newShape));
-
-  
